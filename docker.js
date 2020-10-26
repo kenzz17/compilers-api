d72602970c4fa93ffba46da9352aca22d96d52cc
Aman Singh <amansingh130301@gmail.com>
2020-10-26 22:18:19 +0530

    v1 ready

--- docker.js
+++ docker.js
@@ -6,47 +6,65 @@ function docker_run(filename, lang){
     return new Promise((resolve)=>{
         switch(lang){
             case 'cpp':
-                const docker_cpp = spawn('docker', [
-                    'run', '--rm',
-                    '--stop-timeout', '5',
+                const docker_cpp = spawn('sudo', [
+                    'docker','run', '--rm',
+                    '--memory', '75m',
+                    '--cpus', '0.5',
+                    '--name', filename,
                     '--volume', full_dirname+':/my',
+                    '--init',
                     'run-cpp',
+                    'timeout', '-s', 'SIGKILL', '5',
                     '/bin/sh',
                     '-c', 'g++ /my/main.cpp; ./a.out < /my/in'
-                ]);
+                ],{detached:true});
+                setTimeout(()=>{
+                        resolve({"error":">4 seconds: Time limit exceeded"});
+                }, 4000);
                 docker_cpp.stdout.setEncoding('utf-8');
                 docker_cpp.stderr.setEncoding('utf-8');
                 const resp = {"stdout":"", "stderr":""};
                 docker_cpp.stderr.on('data', (data)=>{resp.stderr += data});
                 docker_cpp.stdout.on('data', (data)=>{
                         resp.stdout += data;
-                        if(resp.stdout.length>10000) resolve({"error":"Stdout limit exceeded", "stderr":resp.stderr, "stdout":resp.stdout});
+                        if(resp.stdout.length>5000){
+                                resolve({"error":">5000 characters: Stdout limit exceeded", "stderr":resp.stderr, "stdout":resp.stdout});
+                        }
                 });
                 docker_cpp.on('exit', ()=>resolve(resp));
                 docker_cpp.on('error', ()=>resolve({"error":"Docker error"}));
                 break;
             case 'py':
-                const docker_py = spawn('docker', [
-                    'run', '--rm',
-                    '--stop-timeout', '5',
+                const docker_py = spawn('sudo', [
+                    'docker','run', '--rm',
+                    '--memory', '75m',
+                    '--cpus', '0.5',
+                    '--name', filename,
                     '--volume', full_dirname+':/my',
+                    '--init',
                     'run-python',
+                    'timeout', '-s', 'SIGKILL', '5',
                     '/bin/sh',
                     '-c', 'python3 /my/main.py < /my/in'
-                ]);
+                ], {detached:true});
+                setTimeout(()=>{
+                        resolve({"error":">4 seconds: Time limit exceeded"});
+                }, 4000);
                 docker_py.stdout.setEncoding('utf-8');
                 docker_py.stderr.setEncoding('utf-8');
                 const resp_ = {"stdout":"", "stderr":""};
                 docker_py.stderr.on('data', (data)=>{resp_.stderr += data});
                 docker_py.stdout.on('data', (data)=>{
                         resp_.stdout += data;
-                        if(resp_.stdout.length>10000) resolve({"error":"Stdout limit exceeded", "stderr":resp_.stderr, "stdout":resp_.stdout});
+                        if(resp_.stdout.length>5000) {
+                                resolve({"error":">5000 characters: Stdout limit exceeded", "stderr":resp_.stderr, "stdout":resp_.stdout});
+                        }
                 });
                 docker_py.on('exit', ()=>resolve(resp_));
                 docker_py.on('error', ()=>resolve({"error":"Docker error"}));
                 break;
             default:
-                resolve({"error":"supported languages are cpp, py"})
+                resolve({"error":"suppoted languages are cpp, py"})
         }
     });
 }
