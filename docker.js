e10dc3cb845bdd0b31a92f2374b584524d4aec17
Aman Singh <amansingh130301@gmail.com>
2020-12-05 19:57:06 +0530

    updated with v2 and v3

--- docker.js
+++ docker.js
@@ -1,53 +1,53 @@
 const { spawn } = require('child_process');
 
-function run (cmd, image_name, filename, full_dirname,resolve){
+function run(cmd, image_name, filename, full_dirname, resolve) {
     const docker_process = spawn('sudo', [
-        'docker','run', '--rm',
+        'docker', 'run', '--rm',
         '--memory', '75m',
         '--cpus', '0.5',
         '--name', filename,
-        '--volume', full_dirname+':/my',
+        '--volume', full_dirname + ':/my',
         '--init',
         image_name,
         'timeout', '-s', 'SIGKILL', '5',
         '/bin/sh',
         '-c', cmd
-    ],{detached:true});
+    ], { detached: true });
     docker_process.stdout.setEncoding('utf-8'); docker_process.stderr.setEncoding('utf-8');
 
-    setTimeout(()=>{
-            resolve({"error":">4 seconds: Time limit exceeded","stdout":"","stderr":""});
+    setTimeout(() => {
+        resolve({ "error": ">4 seconds: Time limit exceeded", "stdout": "", "stderr": "" });
     }, 4000);
 
-    const resp = {"stdout":"", "stderr":"","error":""};
-    docker_process.stderr.on('data', (data)=>{resp.stderr += data});
-    docker_process.stdout.on('data', (data)=>{
-            resp.stdout += data;
-            if(resp.stdout.length>5000){
-                    resolve({"error":">5000 characters: Stdout limit exceeded", "stderr":resp.stderr, "stdout":resp.stdout});
-            }
+    const resp = { "stdout": "", "stderr": "", "error": "" };
+    docker_process.stderr.on('data', (data) => { resp.stderr += data });
+    docker_process.stdout.on('data', (data) => {
+        resp.stdout += data;
+        if (resp.stdout.length > 5000) {
+            resolve({ "error": ">5000 characters: Stdout limit exceeded", "stderr": resp.stderr, "stdout": resp.stdout });
+        }
     });
 
-    docker_process.on('exit', ()=>resolve(resp));
-    docker_process.on('error', ()=>resolve({"error":"Docker error","stdout":"","stderr":""}));
+    docker_process.on('exit', () => resolve(resp));
+    docker_process.on('error', () => resolve({ "error": "Docker error", "stdout": "", "stderr": "" }));
 }
 
-function docker_run(filename, lang){
-    full_dirname = __dirname+'/temp/'+filename+'/';
+function docker_run(filename, toRun, lang) {
+    full_dirname = __dirname + '/temp/' + filename + '/';
 
-    return new Promise((resolve)=>{
-        switch(lang){
+    return new Promise((resolve) => {
+        switch (lang) {
             case 'cpp':
-                run('g++ /my/main.cpp; ./a.out < /my/in', 'run-cpp', filename, full_dirname, resolve);
+                run('g++ "/my/' + toRun + '"; ./a.out < /my/in', 'run-cpp', filename, full_dirname, resolve);
                 break;
             case 'py':
-                run('python3 /my/main.py < /my/in', 'run-python', filename, full_dirname, resolve);
+                run('python3 "/my/' + toRun + '" < /my/in', 'run-python', filename, full_dirname, resolve);
                 break;
             case 'js':
-                run('node /my/main.js < /my/in', 'run-js', filename, full_dirname, resolve);
+                run('node "/my/' + toRun + '" < /my/in', 'run-js', filename, full_dirname, resolve);
                 break;
             default:
-                resolve({"error":"supported languages are cpp, py","stdout":"","stderr":""})
+                resolve({ "error": "supported languages are cpp, py, js", "stdout": "", "stderr": "" })
         }
     });
 }
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const writeFile_promise = util.promisify(require('fs').writeFile);
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: true }));
 
 // CORS
 app.use((req, res, next) => {
@@ -20,36 +20,95 @@ app.use((req, res, next) => {
 
 
 app.get('/', (req, res) => {
-    res.json({"team":"kenzz17","members":["Shivam Raj","Aman Singh", "Pawan Kumar","Aniket Agrawal"]})
+    res.json({ "team": "kenzz17", "members": ["Shivam Raj", "Aman Singh", "Pawan Kumar", "Aniket Agrawal"] })
 })
 
-app.get('/v1', (req, res) => {
-    res.json({"lang":"", "code":"", "stdin":""})
-})
+app.post('/v1', async (req, res) => {
+    if (req.body.passwd != '314159kenzz17') return res.json({ "error": "Access Denied", "stdout": "", "stdout": "" });
 
-app.post('/v1', async (req,res) => {
-    if(req.body.passwd != '314159kenzz17') return res.json({"error":"Access Denied","stdout":"","stdout":""});
     filename = uuid.v4();
+    toRun = '__main__.' + req.body.lang;
     try {
-        spawnSync('sudo', ['mkdir','-p', './temp/'+filename]);
-        const code = writeFile_promise('./temp/'+filename+'/main.'+req.body.lang, req.body.code);
-        const stdin = writeFile_promise('./temp/'+filename+'/in', req.body.stdin);
-        await Promise.all([code, stdin]).catch(()=>{throw 'err'});
+        spawnSync('sudo', ['mkdir', '-p', './temp/' + filename]);
+        const code = writeFile_promise('./temp/' + filename + '/' + toRun, req.body.code);
+        const stdin = writeFile_promise('./temp/' + filename + '/in', req.body.stdin);
+        await Promise.all([code, stdin]).catch((err) => { throw 'err' });
     } catch (err) {
-        spawn('sudo', ['rm','-r', './temp/'+filename]);
-        return res.json({"error":"Unable to write","stderr":"","stdout":""});
+        spawn('sudo', ['rm', '-r', './temp/' + filename]);
+        return res.json({ "error": "Unable to write", "stderr": "", "stdout": "" });
     }
-    docker_run(filename, req.body.lang)
-       .then((data)=>{
-            spawn('sudo', ['rm','-r', './temp/'+filename]);
+
+    docker_run(filename, toRun, req.body.lang)
+        .then((data) => {
+            spawn('sudo', ['rm', '-r', './temp/' + filename]);
             res.json(data);
         })
-       .catch(()=>{
-            spawn('sudo', ['rm','-r', './temp/'+filename]);
-            res.json({"error":"Unable to run/compile", "stderr":"", "stdout":""});
+        .catch(() => {
+            spawn('sudo', ['rm', '-r', './temp/' + filename]);
+            res.json({ "error": "Unable to run/compile", "stderr": "", "stdout": "" });
+        });
+})
+
+app.post('/v2', async (req, res) => {
+    if (req.body.passwd != '314159kenzz17') return res.json({ "error": "Access Denied", "stdout": "", "stdout": "" });
+
+    filename = uuid.v4();
+    toRun = '__main__.' + req.body.lang;
+    try {
+        spawnSync('sudo', ['mkdir', '-p', './temp/' + filename]);
+        const helper = [];
+        req.body.helper.forEach((ele, idx) => {
+            helper.push(writeFile_promise('./temp/' + filename + '/' + ele.name + '.' + ele.lang, ele.body));
+        });
+        const code = writeFile_promise('./temp/' + filename + '/' + toRun, req.body.code);
+        const stdin = writeFile_promise('./temp/' + filename + '/in', req.body.stdin);
+        await Promise.all([...helper, code, stdin]).catch((err) => { throw 'err' });
+    } catch (err) {
+        spawn('sudo', ['rm', '-r', './temp/' + filename]);
+        return res.json({ "error": "Unable to write", "stderr": "", "stdout": "" });
+    }
+
+    docker_run(filename, toRun, req.body.lang)
+        .then((data) => {
+            spawn('sudo', ['rm', '-r', './temp/' + filename]);
+            res.json(data);
+        })
+        .catch(() => {
+            spawn('sudo', ['rm', '-r', './temp/' + filename]);
+            res.json({ "error": "Unable to run/compile", "stderr": "", "stdout": "" });
+        });
+})
+
+app.post('/v3', async (req, res) => {
+    if (req.body.passwd != '314159kenzz17') return res.json({ "error": "Access Denied", "stdout": "", "stdout": "" });
+
+    filename = uuid.v4();
+    toRun = req.body.path + '/' + req.body.name + '.' + req.body.lang;
+    try {
+        const helper = [];
+        req.body.helper.forEach((ele, idx) => {
+            file = './temp/' + filename + '/' + ele.path + '/' + ele.name + '.' + ele.lang;
+            dir = './temp/' + filename + '/' + ele.path + '/';
+            spawnSync('sudo', ['mkdir', '-p', dir]);
+            helper.push(writeFile_promise(file, ele.body));
+        });
+        const stdin = writeFile_promise('./temp/' + filename + '/in', req.body.stdin);
+        await Promise.all([...helper, stdin]).catch((err) => { throw 'err' });
+    } catch (err) {
+        spawn('sudo', ['rm', '-r', './temp/' + filename]);
+        return res.json({ "error": "Unable to write", "stderr": "", "stdout": "" });
+    }
+
+    docker_run(filename, toRun, req.body.lang)
+        .then((data) => {
+            spawn('sudo', ['rm', '-r', './temp/' + filename]);
+            res.json(data);
+        })
+        .catch(() => {
+            spawn('sudo', ['rm', '-r', './temp/' + filename]);
+            res.json({ "error": "Unable to run/compile", "stderr": "", "stdout": "" });
         });
 })
 
 const PORT = 5000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
